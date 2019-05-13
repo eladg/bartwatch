6393eb8487fb448ab1915b4cb0c2b9bd055288ae
Aviv Bueno <aviv.buen@gmail.com>
2019-05-14 00:20:21 +0300

    Update deploy-dev-to-s3.sh

--- scripts/deploy-dev-to-s3.sh
+++ scripts/deploy-dev-to-s3.sh
@@ -2,7 +2,7 @@
 
 # ============================  Script Variables   ============================
 DEPLOY_BUCKET="bartdev.gariany.com"
-PUBLIC_FOLDER="./public"
+PUBLIC_FOLDER="../public"
 PUBLIC_URL="http://bartdev.gariany.com"
 # =============================================================================
 
