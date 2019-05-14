0f65b19e56f042c77b6668ca7468d2935793bb7e
Aviv Bueno <aviv.buen@gmail.com>
2019-05-14 14:08:44 +0300

    Sampler Test

---
+++ src/js/audio.js
@@ -0,0 +1,15 @@
+const sounds = {
+    HIGH_DING: 'C3',
+    LOW_DING: 'D3',
+    WOOSH: 'E3',
+}
+
+var sampler = new Tone.Sampler({
+    "C3" : "./audio/high-ding.mp3",
+    "D3" : "./audio/low-ding.mp3",
+    "E3" : "./audio/woosh.mp3",
+});
+
+function playSampler(string_of_the_sound_to_play){
+    sampler.triggerAttack(string_of_the_sound_to_play);
+}
